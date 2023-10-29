8bcbc3f29f4b9a5d64bd1ad0884402c1eafa5ffe
thwldus <gee906@naver.com>
2023-10-29 22:15:12 +0900

    ALbumFragment_TabLayout과 ViewPager2 연결

--- app/src/main/java/com/example/flo/AlbumFragment.kt
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -9,9 +9,12 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
 import com.example.flo.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
     lateinit var binding : FragmentAlbumBinding
+
+    private val information = arrayListOf("수록곡","상세정보", "영상")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +34,10 @@ class AlbumFragment : Fragment() {
 
         val albumAdapter = AlbumVPAdapter(this)
         binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp){
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
 
         return binding.root
     }
