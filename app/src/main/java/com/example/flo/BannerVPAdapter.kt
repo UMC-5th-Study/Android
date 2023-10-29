3a4ef902e69e996788e6aa0eb0231c47ce5e8d23
thwldus <gee906@naver.com>
2023-10-29 17:39:04 +0900

    HomeFragment의 배너 부분 ImageView → ViewPager2

---
+++ app/src/main/java/com/example/flo/BannerVPAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.flo
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class BannerVPAdapter(fragment:Fragment) : FragmentStateAdapter(fragment){
+
+    private val fragmentlist :ArrayList<Fragment> = ArrayList()
+
+    /*override fun getItemCount(): Int {
+        return fragmentlist.size
+    }*/
+    override fun getItemCount(): Int = fragmentlist.size
+
+    override fun createFragment(position: Int): Fragment = fragmentlist[position]
+
+    fun addFragment(fragment: Fragment){
+        fragmentlist.add(fragment)
+        notifyItemInserted(fragmentlist.size-1) //새로운 값이 추가되었음을 뷰페이저에게 안내
+    }
+}
--- app/src/main/java/com/example/flo/HomeFragment.kt
+++ app/src/main/java/com/example/flo/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -21,6 +22,12 @@ class HomeFragment : Fragment() {
         binding.homeAlbumImgIv1.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
         }
+
+        //Adapter를 통해 데이터를 가져옴
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment())
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         return binding.root
     }
