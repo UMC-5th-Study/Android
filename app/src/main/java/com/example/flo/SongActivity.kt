680a4810af57833b0b808acd61a53fd0c870839b
thwldus <gee906@naver.com>
2023-10-31 23:40:00 +0900

    Song에 대한 추가정보(진행시간, 총 시간, 재생여부) 연결

--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -8,11 +8,15 @@ import com.example.flo.databinding.ActivitySongBinding
 class SongActivity : AppCompatActivity() {
 
     lateinit var binding : ActivitySongBinding
+    lateinit var song : Song
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initSong()
+        setPlayer(song)
 
         binding.songDownIb.setOnClickListener {
             finish()
@@ -24,12 +28,30 @@ class SongActivity : AppCompatActivity() {
         binding.songPauseIv.setOnClickListener {
             setPlayerStatus(true)
         }
+
+    }
+
+    private fun initSong(){
         if(intent.hasExtra("title")&&intent.hasExtra("singer")){
-            binding.songMusicTitleTv.text = intent.getStringExtra("title")
-            binding.songSingerNameTv.text = intent.getStringExtra("singer")
+            song = Song(
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("singer")!!,
+                intent.getIntExtra("second", 0),
+                intent.getIntExtra("playTime", 0),
+                intent.getBooleanExtra("isPlaying", false)
+            )
         }
     }
-    
+    private fun setPlayer(song: Song){
+        binding.songMusicTitleTv.text = intent.getStringExtra("title")
+        binding.songSingerNameTv.text = intent.getStringExtra("singer")
+        binding.songStartTimeTv.text = String.format("%02d:%02d", song.second/60, song.second%60)
+        binding.songEndTimeTv.text = String.format("%02d:%02d", song.playTime/60, song.playTime%60)
+        binding.songProgressSb.progress = (song.second*1000/song.playTime)
+
+        setPlayerStatus(song.isPlaying)
+    }
+
     fun setPlayerStatus(isPlaying : Boolean){
         if(isPlaying){
             binding.songMiniplayerIv.visibility = View.VISIBLE
