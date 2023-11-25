3c0f846de2fda86728d1791519891670282c40c4
thwldus <gee906@naver.com>
2023-11-25 23:53:45 +0900

    [week7] DB를 이용하여 앱 시작, 종료시 저장된 음악 상태 실행 및 저장 + SongActivity로 전환 시 SongId 이용

--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -11,18 +11,21 @@ import com.google.gson.Gson
 class SongActivity : AppCompatActivity() {
 
     lateinit var binding : ActivitySongBinding
-    lateinit var song : Song
     lateinit var timer : Timer
     private var mediaPlayer: MediaPlayer? = null
     private var gson: Gson = Gson()
+
+    val songs = arrayListOf<Song>()
+    lateinit var songDB: SongDatabase
+    var nowPos = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initPlayList()
         initSong()
-        setPlayer(song)
 
         binding.songDownIb.setOnClickListener {
             finish()
@@ -37,24 +40,37 @@ class SongActivity : AppCompatActivity() {
 
     }
 
-    private fun initSong(){
-        if(intent.hasExtra("title")&&intent.hasExtra("singer")){
-            song = Song(
-                intent.getStringExtra("title")!!,
-                intent.getStringExtra("singer")!!,
-                intent.getIntExtra("second", 0),
-                intent.getIntExtra("playTime", 0),
-                intent.getBooleanExtra("isPlaying", false),
-                intent.getStringExtra("music")!!
-            )
-        }
-        startTimer()
+    private fun initPlayList(){
+        songDB = SongDatabase.getInstance(this)!!
+        songs.addAll(songDB.songDao().getSongs())
     }
+
+    private fun initSong(){
+        val spf = getSharedPreferences("song", MODE_PRIVATE)
+        val songId = spf.getInt("songId", 0)
+
+        nowPos = getPlayingSongPosition(songId)
+
+        Log.d("now Song ID",songs[nowPos].id.toString())
+        startTimer()
+        setPlayer(songs[nowPos])
+    }
+
+    private fun getPlayingSongPosition(songId: Int): Int{
+        for (i in 0 until songs.size){
+            if (songs[i].id == songId){
+                return i
+            }
+        }
+        return 0
+    }
+
     private fun setPlayer(song: Song){
-        binding.songMusicTitleTv.text = intent.getStringExtra("title")
-        binding.songSingerNameTv.text = intent.getStringExtra("singer")
+        binding.songMusicTitleTv.text = song.title
+        binding.songSingerNameTv.text = song.singer
         binding.songStartTimeTv.text = String.format("%02d:%02d", song.second/60, song.second%60)
         binding.songEndTimeTv.text = String.format("%02d:%02d", song.playTime/60, song.playTime%60)
+        binding.songAlbumIv.setImageResource(song.coverImg!!)
         binding.songProgressSb.progress = (song.second*1000/song.playTime)
         val music = resources.getIdentifier(song.music, "raw", this.packageName)
         mediaPlayer = MediaPlayer.create(this, music)
@@ -62,7 +78,7 @@ class SongActivity : AppCompatActivity() {
     }
 
     fun setPlayerStatus(isPlaying : Boolean){
-        song.isPlaying = isPlaying
+        songs[nowPos].isPlaying = isPlaying
         timer.isPlaying = isPlaying
 
         if(isPlaying){
@@ -80,7 +96,7 @@ class SongActivity : AppCompatActivity() {
     }
 
     private fun startTimer(){
-        timer = Timer(song.playTime,song.isPlaying)
+        timer = Timer(songs[nowPos].playTime,songs[nowPos].isPlaying)
         timer.start()
     }
 
@@ -94,12 +110,12 @@ class SongActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         setPlayerStatus(false)
-        song.second = ((binding.songProgressSb.progress * song.playTime)/100)/1000
+        songs[nowPos].second = ((binding.songProgressSb.progress * songs[nowPos].playTime)/100)/1000
+
         val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
         //sharedPreferneces 데이터 조작 시, 에디터 생성해야 함!
         val editor = sharedPreferences.edit()
-        val songJson = gson.toJson(song)
-        editor.putString("songData", songJson)
+        editor.putInt("songId", songs[nowPos].id)
 
         editor.apply()
     }
