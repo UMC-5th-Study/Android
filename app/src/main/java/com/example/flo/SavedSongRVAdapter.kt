9583f227884604ddbac6e2c64fab7c9de69517de
thwldus <gee906@naver.com>
2023-11-26 14:31:35 +0900

    [week7] 보관함 좋아요 표시한 song 데이터로 구성 및 제거

--- app/src/main/java/com/example/flo/SavedSongRVAdapter.kt
+++ app/src/main/java/com/example/flo/SavedSongRVAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.flo
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,6 +9,14 @@ import com.example.flo.databinding.ItemSongBinding
 class SavedSongRVAdapter() : RecyclerView.Adapter<SavedSongRVAdapter.ViewHolder>(){
 
     private val songs = ArrayList<Song>()
+
+    interface MyItemClickListener{
+        fun onRemoveSong(songId: Int)
+    }
+    private lateinit var mItemClickListener: MyItemClickListener
+    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
+        mItemClickListener = itemClickListener
+    }
 
     override fun onCreateViewHolder(
         viewGroup: ViewGroup,
@@ -20,9 +29,27 @@ class SavedSongRVAdapter() : RecyclerView.Adapter<SavedSongRVAdapter.ViewHolder>
 
     override fun onBindViewHolder(holder: SavedSongRVAdapter.ViewHolder, position: Int) {
         holder.bind(songs[position])
+        holder.binding.itemSongMoreIv.setOnClickListener {
+            mItemClickListener.onRemoveSong(songs[position].id)
+            removeSong(position)
+        }
     }
 
     override fun getItemCount(): Int = songs.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun addSongs(songs: ArrayList<Song>) {
+        this.songs.clear()
+        this.songs.addAll(songs)
+
+        notifyDataSetChanged()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun removeSong(position: Int){
+        songs.removeAt(position)
+        notifyDataSetChanged()
+    }
 
     inner class ViewHolder(val binding: ItemSongBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(song: Song){
