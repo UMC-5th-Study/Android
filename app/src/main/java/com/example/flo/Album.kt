6071170010dd23eee855549e440df27d08f5cdb2
thwldus <gee906@naver.com>
2023-11-28 10:41:43 +0900

    [week7+] Album DB 구현하고 적용하기

--- app/src/main/java/com/example/flo/Album.kt
+++ app/src/main/java/com/example/flo/Album.kt
@@ -1,10 +1,13 @@
 package com.example.flo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity(tableName = "AlbumTable")
 data class Album(
+    @PrimaryKey(autoGenerate = false) var id: Int = 0,
     var title: String? = "",
     var singer: String? = "",
-    var coverImg: Int? = null,
-    var songs: ArrayList<Song>? = null //수록곡정보 - 강의에서는 활용X
+    var coverImg: Int? = null
 )
---
+++ app/src/main/java/com/example/flo/AlbumDao.kt
@@ -0,0 +1,18 @@
+package com.example.flo
+
+import androidx.room.*
+
+@Dao
+interface AlbumDao {
+    @Insert
+    fun insert(album: Album)
+
+    @Update
+    fun update(album: Album)
+
+    @Delete
+    fun delete(album: Album)
+
+    @Query("SELECT * FROM AlbumTable")
+    fun getAlbums(): List<Album>
+}
