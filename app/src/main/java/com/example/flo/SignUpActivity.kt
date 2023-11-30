3a7773000567fc5291d7c752c7141db993acdfed
thwldus <gee906@naver.com>
2023-11-30 22:04:39 +0900

    [week8] 사용자 DB 구현 + 버튼 클릭 시 회원가입 진행

--- app/src/main/java/com/example/flo/SignUpActivity.kt
+++ app/src/main/java/com/example/flo/SignUpActivity.kt
@@ -1,6 +1,8 @@
 package com.example.flo
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySignupBinding
 
@@ -11,5 +13,36 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.signUpSignUpBtn.setOnClickListener{
+            signUp()
+            finish()
+        }
+    }
+
+    private fun getUser(): User{
+        val email: String = binding.signUpIdEt.text.toString() + "@" + binding.signUpDirectInputEt.text.toString()
+        val pwd: String = binding.signUpPasswordEt.text.toString()
+
+        return User(email, pwd)
+    }
+
+    private fun signUp() {
+        //정보 입력이 미흡하거나 비밀번호 재확인이 일치하지 않는다면 회원가입 진행 X
+        if (binding.signUpIdEt.text.toString().isEmpty() || binding.signUpDirectInputEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (binding.signUpPasswordEt.text.toString() != binding.signUpPasswordCheckEt.text.toString()) {
+            Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //오류가 없는 경우 회원가입 진행 → 입력정보를 DB에 저장
+        val userDB = SongDatabase.getInstance(this)!!
+        userDB.userDao().insert(getUser())
+
+        val user = userDB.userDao().getUsers()
+        Log.d("SIGNUPACT", user.toString())
     }
 }
