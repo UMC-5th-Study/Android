5f4876b4b8fe7bc6d870a25c8c84d9e517d03517
thwldus <gee906@naver.com>
2023-12-20 13:51:22 +0900

    [week9] 회원가입 이름 정보 추가 + Retrofit를 통한 회원가입

---
+++ app/src/main/java/com/example/flo/AuthRetrofitInterface.kt
@@ -0,0 +1,11 @@
+package com.example.flo
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthRetrofitInterface {
+    @POST("/users")
+    fun signUp(@Body user: User): Call<AuthResponse>
+
+}
--- app/src/main/java/com/example/flo/SignUpActivity.kt
+++ app/src/main/java/com/example/flo/SignUpActivity.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySignupBinding
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignupBinding
@@ -23,11 +26,12 @@ class SignUpActivity : AppCompatActivity() {
     private fun getUser(): User{
         val email: String = binding.signUpIdEt.text.toString() + "@" + binding.signUpDirectInputEt.text.toString()
         val pwd: String = binding.signUpPasswordEt.text.toString()
+        val name: String = binding.signUpNameEt.text.toString()
 
-        return User(email, pwd)
+        return User(email, pwd, name)
     }
 
-    private fun signUp() {
+    /*private fun signUp() {
         //정보 입력이 미흡하거나 비밀번호 재확인이 일치하지 않는다면 회원가입 진행 X
         if (binding.signUpIdEt.text.toString().isEmpty() || binding.signUpDirectInputEt.text.toString().isEmpty()){
             Toast.makeText(this, "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
@@ -44,5 +48,35 @@ class SignUpActivity : AppCompatActivity() {
 
         val user = userDB.userDao().getUsers()
         Log.d("SIGNUPACT", user.toString())
+    }*/
+
+    private fun signUp() {
+        //정보 입력이 미흡하거나 비밀번호 재확인이 일치하지 않는다면 회원가입 진행 X
+        if (binding.signUpIdEt.text.toString().isEmpty() || binding.signUpDirectInputEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (binding.signUpNameEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이름 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (binding.signUpPasswordEt.text.toString() != binding.signUpPasswordCheckEt.text.toString()) {
+            Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //네트워크를 통한 회원가입
+        val authService = getRetrofit().create(AuthRetrofitInterface::class.java)
+        authService.signUp(getUser()).enqueue(object: retrofit2.Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                Log.d("SIGNUP/SUCCESS", response.toString())
+            }
+
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Log.d("SIGNUP/FAILURE", t.message.toString())
+            }
+
+        })
+        Log.d("SIGNUP", "HELLO")
     }
 }
